69e66748abc3bb7795f822b4887390725ebf1031
HyunsunHong <121751674+HyunsunHong@users.noreply.github.com>
2023-01-08 00:49:48 +0900

    dp, top-down
    전형적인 지도맵에서의 top-down dp문제.
    
    0. cache[r][c][milk]의 의미는 '(r, c)상에서 최근 마신 우유가 milk일때 마신 우유의 최대 값'
    
    1. 우유를 마실때딸기우유가 아닌 경우 이전 우유를 무조건 마신 상태여야만 한다 (37번줄)
    
    2. 해당 우유를 마실 수 있을 때와 마실 수 없을 때를 구별해서 비교해야한다. (34, 42, 43번 줄)

--- BOJ/14722.cpp
+++ BOJ/14722.cpp
@@ -1,10 +1,27 @@
+/*
+
+gold 4
+
+dp, top-down using cache
+
+전형적인 지도맵에서의 top-down dp문제.
+
+0. cache[r][c][milk]의 의미는 '(r, c)상에서 최근 마신 우유가 milk일때 마신 우유의 최대 값'
+
+1. 우유를 마실때딸기우유가 아닌 경우 이전 우유를 무조건 마신 상태여야만 한다 (45번줄)
+
+2. 해당 우유를 마실 수 있을 때와 마실 수 없을 때를 구별해서 비교해야한다. (42, 50, 51번 줄)
+
+*/
+
+
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
 int N;
-int cache[1001][1001][4]; // 
+int cache[1001][1001][3]; // 0 ~ 2(recently drunk)
 int map[1001][1001];
 int ans = 0;
 
@@ -22,33 +39,18 @@ int solve(int r, int c, int milk) {
 
     int ret = 0;
 
-    if(milk <= 3) {
-        if(map[r][c] == milk) {
-            ret =  max(solve(r - 1, c, pre_milk(milk)), solve(r, c - 1, pre_milk(milk)));
-            
-            if(milk == 0) {
-                ret++;
-            }
-            else {
-                if(ret != 0) {
-                    ret++;
-                }
-            }
+    if(map[r][c] == milk) {
+        ret = max(solve(r - 1, c, pre_milk(milk)), solve(r, c - 1, pre_milk(milk)));
 
-            return cache[r][c][milk] = ret;
-        }
-        else {
-            return cache[r][c][milk] = 0;
+        if(milk == 0 || ret != 0) {
+            ret++;
         }
     }
-    else {
-        for(int i = 0; i <= 3; i++) {
-            ret = max(ret, solve(r - 1, c, i));
-            ret = max(ret, solve(r, c - 1, i));
-        }
 
-        return cache[r][c][milk] = ret;
-    }
+    ret = max(ret, solve(r - 1, c, milk));
+    ret = max(ret, solve(r, c - 1, milk));
+
+    return cache[r][c][milk] = ret;
 }
 
 int main() {
@@ -66,7 +68,6 @@ int main() {
     ans = max(ans, solve(N, N, 0));
     ans = max(ans, solve(N, N, 1));
     ans = max(ans, solve(N, N, 2));
-    ans = max(ans, solve(N, N, 3));
 
     cout << ans << "\n";
 
